3f617d9243afb310fc7754d4951791fa00528ec9
Jongkuch1 <j.anyar@alustudent.com>
2024-11-10 19:14:26 +0200

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-read_file = __import__('0-read_file').read_file
-
-read_file("my_file_0.txt")
+read_file = __import
+def read_file(filename=""):
+    """
+    Reads a text file (UTF-8) and prints it to stdout.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
 
