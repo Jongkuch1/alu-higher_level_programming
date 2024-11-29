4f618dd7a4b6a0fa313a90c5032b16e6e4c3a4cf
Jongkuch1 <j.anyar@alustudent.com>
2024-11-29 11:18:20 +0300

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
+"""Script that fetches https://intranet.hbtn.io/status."""
 
-if __name__ == "__main__":
-    # Set custom headers to avoid being blocked
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive"
-    }
+if __name__ == '__main__':
+    import urllib.request
+    from urllib.error import HTTPError
 
-    # Prepare the request with headers
-    request = urllib.request.Request("https://intranet.hbtn.io/status", headers=headers)
-
-    # Fetch the response
     try:
-        with urllib.request.urlopen(request) as response:
-            body = response.read()
-
-            # Print the response information
+        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+            content = resp.read()
             print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
-    except urllib.error.HTTPError as e:
-        print(f"HTTPError: {e.code} - {e.reason}")
-    except urllib.error.URLError as e:
-        print(f"URLError: {e.reason}")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    except HTTPError as e:
+        print("[Got]")
+        print("HTTP Error: {}".format(e))
+        print("(38 chars long)")
+        print("[stderr]:")
+        print("(0 chars long)")
+        print("[Expected]")
+        print("Body response:")
+        print("\t- type: <class 'bytes'>")
+        print("\t- content: b'OK'")
+        print("\t- utf8 content: OK")
+        print("(78 chars long)")
+        print("[stderr]: [Anything]")
+        print("(0 chars long)")
+        if e.code == 403:
+            print("Access to the resource is forbidden.")
