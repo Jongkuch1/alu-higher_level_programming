40e3a1807bdf1a8479046a391ca0fe61b82622ef
Jongkuch1 <j.anyar@alustudent.com>
2024-11-29 11:12:51 +0300

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""a Python script that fetches"""
-
+"""Fetche https://intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-    reply = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(reply.text)))
-    print("\t- content: {}".format(reply.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
